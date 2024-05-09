1b8600571b8f26abb1c109d36eb191d35a23a67d
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-09 20:41:39 +0300

    Adds Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__content">
+          <p className="footer__text">© 2024 Realtor, Inc.</p>
+          <p className="footer__text">Contact us: contact@realtor.com</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
