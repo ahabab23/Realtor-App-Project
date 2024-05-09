9ed86cc85edd429b54561c8e3f5c7cd64c4f91f1
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-09 20:45:59 +0300

    Adds loading message as properties are loaded

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -1,56 +1,67 @@
 import React from "react";
-import "./index.css";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
-import DefaultImage from "../../Images/home.jpg";
 
-function Property({
+import DefaultImage from "../../Images/home.jpg";
+import "./index.css"; // Import your vanilla CSS file
+
+const Property = ({
   property: {
     coverPhoto,
     price,
-    rentalFrequency,
+    rentFrequency,
     rooms,
     title,
     baths,
     area,
     agency,
     isVerified,
-    externalId,
+    externalID,
   },
-}) {
-  return (
-    <div className="property">
-      <div>
+}) => (
+  <a href={`/property/${externalID}`} className="property-link">
+    <div className="property-container">
+      <div className="property-image">
         <img
           src={coverPhoto ? coverPhoto.url : DefaultImage}
-          alt="homes"
+          alt="Property"
           width={400}
           height={260}
         />
       </div>
-      <div>
-        <div>
-          <div>
-            <div>{isVerified && <GoVerified />}</div>
-            <h1>
-              AED {millify(price)}
-              {rentalFrequency && `/$ rentalFrequency`}
-            </h1>
-          </div>
-          <div>
-            {" "}
-            <img src={agency?.logo?.url} alt="avatar" />
-          </div>
+      <div className="property-details">
+        <div className="property-header">
+          {isVerified && <GoVerified className="verified-icon" />}
+          <span className="property-price">
+            AED {millify(price)}
+            {rentFrequency && `/${rentFrequency}`}
+          </span>
+          <img
+            src={agency?.logo?.url}
+            alt="Agency Logo"
+            className="agency-logo"
+          />
         </div>
-        <div>
-          {rooms} <FaBed />|{baths}
-          <FaBath />|{millify(area)} sqft <BsGridFill />
-          <h1>{title.length > 30 ? `${title.substring(0, 30)}...` : title}</h1>
+        <div className="property-meta">
+          <span className="property-room">
+            {rooms} BR <FaBed />
+          </span>
+          <span className="property-bath">
+            {baths} BATH
+            <FaBath />
+          </span>
+          <span className="property-area">
+            {millify(area)} sqft <BsGridFill />
+          </span>
+        </div>
+        <div className="property-title">
+          {title.length > 50 ? `${title.substring(0, 30)}...` : title}
         </div>
       </div>
     </div>
-  );
-}
+  </a>
+);
+
 export default Property;
