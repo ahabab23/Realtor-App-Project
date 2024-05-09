592e16d0db47f45e7b08cd9139b37a5702bff050
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-09 20:50:59 +0300

    Adds Loading state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import Banner from "./components/Banner/Banner";
 import { baseUrl, fetchApi } from "./fetchApi";
 import Property from "./components/Property/Property";
-
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import "./index.css";
 function App({
   propertiesForSale: propsForSale,
   propertiesForRent: propsForRent,
 }) {
   const [propertiesForSale, setPropertiesForSale] = useState([]);
   const [propertiesForRent, setPropertiesForRent] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
+
   // console.log(propertiesForSale);
   // console.log(propertiesForRent);
   useEffect(() => {
@@ -23,16 +27,17 @@ function App({
 
         setPropertiesForSale(propertyForSaleResponse?.hits);
         setPropertiesForRent(propertyForRentResponse?.hits);
+        setLoading(false); // Set loading to false after properties are loaded
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []);
-
+  }, [propertiesForRent, propertiesForSale]);
   return (
     <div>
+      <Navbar />
       <Banner
         purpose="RENT A HOME"
         title1="Rental Homes for"
@@ -43,11 +48,16 @@ function App({
         linkName="/search?purpose-for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       />
-      <div className="property">
-        {propertiesForRent.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </div>
+      {loading ? (
+        <h1 className="propertyHeader">LOADING...</h1> // Display "Loading..." while properties are loading
+      ) : (
+        <div className="property">
+          {propertiesForRent.map((property) => (
+            <Property property={property} key={property.id} />
+          ))}
+        </div>
+      )}
+
       <Banner
         purpose="BUY A HOME"
         title1="Find, Buy and Own Your"
@@ -58,11 +68,16 @@ function App({
         linkName="/search?purpose-for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <div className="property">
-        {propertiesForSale.map((property) => (
-          <Property property={property} key={property.id} />
-        ))}
-      </div>
+      {loading ? (
+        <h1 className="propertyHeader">LOADING...</h1>
+      ) : (
+        <div className="property">
+          {propertiesForSale.map((property) => (
+            <Property property={property} key={property.id} />
+          ))}
+        </div>
+      )}
+      <Footer />
     </div>
   );
 }
