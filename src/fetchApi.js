7ed2bb4881648c5bfa9034aecb66cce850543f8b
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-13 01:14:13 +0300

    adds apis

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -4,13 +4,13 @@ const baseUrl = "https://bayut.p.rapidapi.com"; // Make sure to include the prot
 
 const fetchApi = async (url) => {
   try {
-    const response = await axios.get(url, {
+    const { data } = await axios.get(url, {
       headers: {
-        "X-RapidAPI-Key": "cd2f144024mshccf439fc1e55a48p1aad54jsn08d23813d46c",
+        "X-RapidAPI-Key": "484ba3b1efmsh047d679fb09f469p1234cdjsnc0911648d43c",
         "X-RapidAPI-Host": "bayut.p.rapidapi.com",
       },
     });
-    return response.data; // Return the data from the response
+    return data; // Return the data from the response
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error; // Re-throw the error to handle it elsewhere if needed
